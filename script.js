b1ad95854a7565f69760b28bef266771dbeb6956
val23mejias-ship-it <val23mejias@gmail.com>
2026-02-08 22:49:18 -0600

    jjj

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const fotos = document.querySelectorAll(".slider img");
 let index = 0;
 
-setInterval(() => {
+// precargar imágenes
+fotos.forEach(img => {
+    const preload = new Image();
+    preload.src = img.src;
+});
+
+// mostrar la primera inmediatamente
+fotos[0].classList.add("activa");
+
+function cambiarFoto() {
     fotos[index].classList.remove("activa");
     index = (index + 1) % fotos.length;
     fotos[index].classList.add("activa");
-}, 5000);
+}
+
+// empezar sin espera
+setInterval(cambiarFoto, 4000);
